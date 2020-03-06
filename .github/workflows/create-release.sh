97299b232bedd49b25a102f4235635706f087a4d
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2020-03-06 09:33:14 -0500

    As of 2020-03-06

--- .github/workflows/create-release.sh
+++ .github/workflows/create-release.sh
@@ -2,7 +2,7 @@
 
 archive="unix.confs.zip"
 git archive -o $archive HEAD
-zip -d $archive .gitmodules .github\*
+zip -d $archive .gitmodules
 
 #shellcheck disable=SC2016
 git submodule -q foreach 'cd $toplevel; zip -ru '$archive' $path -x $path/.git/\* $path/.github/\* $path/.git $path/.github'
@@ -10,10 +10,10 @@ git submodule -q foreach 'cd $toplevel; zip -ru '$archive' $path -x $path/.git/\
 tag="${GITHUB_REF}"
 today="$(date "+%Y-%m-%d")"
 name="Release #$tag -- $today"
-description="$(git log -n1 --format='%b')"
+description="$(git log -n1 --format='%s%b')"
 
 # shellcheck disable=SC2154
-auth="Authorization: token ${{ secrets.access_token }}"
+auth="Authorization: token ${access_token} "
 length="Content-Length: $(stat -c%s $archive)"
 type="Content-Type: application/zip"
 
@@ -21,10 +21,11 @@ create_url="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
 
 printf -v json '{"tag_name": "%s", "name":"%s", "body":"%s"}' "$tag" "$name" "$description"
 
+
 upload_url=$(curl -s -H "$auth" -d "$json" "$create_url" | jq -r '.upload_url')
 upload_url="${upload_url%\{*}"
 
 curl -f -sSL -XPOST \
      -H "$auth" -H "$length" -H "$type" \
-     --upload-file "$archive"
-     "$upload_url?name=$archive&label=bare%20files"
+     --upload-file "$archive" \
+     "$upload_url?name=${archive}&label=unix.confs.bare.zip"
